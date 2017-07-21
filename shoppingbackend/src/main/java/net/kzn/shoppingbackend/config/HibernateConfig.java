7fc1a0f9cee840e01335427b4238a9b7fdb0f71c
ghallabiHoussem <houssemghallabi@gmail.com>
2017-07-22 00:09:15 +0200

    update

---
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/config/HibernateConfig.java
@@ -0,0 +1,17 @@
+package net.kzn.shoppingbackend.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages="net.kzn.shoppingbackend.dto")
+@EnableTransactionManagement
+public class HibernateConfig {
+
+	
+	
+	
+	
+	
+}
